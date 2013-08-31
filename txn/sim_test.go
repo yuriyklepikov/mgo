570d4c3615781c0a96fe887430c2c9040f56d982
yuriyklepikov <jurius@ya.ru>
2013-08-31 18:35:21 +0600

    Update sim_test.go

--- txn/sim_test.go
+++ txn/sim_test.go
@@ -2,9 +2,9 @@ package txn_test
 
 import (
 	"flag"
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
-	"labix.org/v2/mgo/txn"
+	"github.com/yuriyklepikov/mgo"
+	"github.com/yuriyklepikov/mgo/bson"
+	"github.com/yuriyklepikov/mgo/txn"
 	. "launchpad.net/gocheck"
 	"math/rand"
 	"time"
