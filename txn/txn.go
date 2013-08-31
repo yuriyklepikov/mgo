e9d43c76edfe5c7160e9188ebefca0582f560d64
yuriyklepikov <jurius@ya.ru>
2013-08-31 18:36:52 +0600

    Update txn.go

--- txn/txn.go
+++ txn/txn.go
@@ -9,8 +9,8 @@ package txn
 import (
 	"encoding/binary"
 	"fmt"
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
+	"github.com/yuriyklepikov/mgo"
+	"github.com/yuriyklepikov/mgo/bson"
 	"reflect"
 	"sort"
 	"sync"
